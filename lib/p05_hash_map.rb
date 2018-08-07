9b955d5cd9e92358a49664916b4bcaa87f9a132b
Shashank Racherla <shashank.racherla@gmail.com>
2018-08-07 13:54:48 -0700

    hashmap

--- lib/p05_hash_map.rb
+++ lib/p05_hash_map.rb
@@ -1,26 +1,51 @@
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
   attr_reader :count
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
     @count = 0
+    @num_buckets = num_buckets
   end
 
   def include?(key)
+    hashed = key.hash
+    bucket = hashed % @num_buckets
+      @store[bucket].include?(key)
+    
   end
 
   def set(key, val)
+    resize! if @count >= num_buckets
+    hashed = key.hash
+    bucket = hashed % @num_buckets
+    if @store[bucket].include?(key)
+      @store[bucket].update(key,val)
+    else
+      @store[bucket].append(key,val)
+      @count += 1
+    end
   end
 
   def get(key)
-  end
+    hashed = key.hash
+    bucket = hashed % @num_buckets
+    @store[bucket].get(key)
+    end
 
   def delete(key)
+    hashed = key.hash
+    bucket = hashed % @num_buckets
+    @store[bucket].remove(key)
+    @count -= 1
   end
 
   def each
+    @store.each do |bucket|
+      bucket.each { |link| yield [link.key, link.val] }
+    end
   end
 
   # uncomment when you have Enumerable included
@@ -41,9 +66,17 @@ class HashMap
   end
 
   def resize!
+     old_store = @store
+    @store = Array.new(num_buckets * 2) { LinkedList.new }
+    @count = 0
+
+    old_store.each do |bucket|
+      bucket.each { |link| set(link.key, link.val) }
+    end
   end
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
 end
